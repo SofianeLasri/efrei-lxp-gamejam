10f62a036c1ec40da01066d7e8c136a860af844d
SofianeLasri <alasri250@gmail.com>
2025-04-14 14:24:42 +0200

    Implement player movement controls using the new Input System

--- Assets/Scripts/Example.cs
+++ Assets/Scripts/Example.cs
@@ -1,16 +1,38 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class Example : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    [Header("Contrôles")]
+    private PlayerInput _playerInput;
+
+    private InputAction _leftMove;
+    private InputAction _rightMove;
+    [SerializeField] private Vector2 _leftMoveValue;
+    [SerializeField] private Vector2 _rightMoveValue;
+    
+    // Awake se lance avant Start
+    void Awake()
     {
-        Debug.Log("Hello, World!");
+        // On récupère l'instance de PlayerInput (à ajouter à la main dans l'inspecteur)
+        _playerInput = GetComponent<PlayerInput>();
+        
+        // On récupère les actions de mouvement gauche et droite
+        _leftMove = _playerInput.currentActionMap.FindAction("LeftMove");
+        
+        // Maintenant on définie la valeur la variable _leftMoveValue
+        _leftMove.performed += ctx => _leftMoveValue = ctx.ReadValue<Vector2>();
+        _leftMove.canceled += ctx => _leftMoveValue = Vector2.zero;
+        
+        // On répète :)
+        _rightMove = _playerInput.currentActionMap.FindAction("RightMove");
+        _rightMove.performed += ctx => _rightMoveValue = ctx.ReadValue<Vector2>();
+        _rightMove.canceled += ctx => _rightMoveValue = Vector2.zero;
     }
 
-    // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+        Debug.Log($"Left Move: {_leftMoveValue}");
+        Debug.Log($"Right Move: {_rightMoveValue}");
     }
 }
