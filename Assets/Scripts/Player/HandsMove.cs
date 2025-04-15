fedde8a645a37e999d9ca178dac7aed3e0c8e02a
SofianeLasri <alasri250@gmail.com>
2025-04-15 10:03:08 +0200

    Refactor classes to use Player namespace and improve code organization

--- Assets/Scripts/Player/HandsMove.cs
+++ Assets/Scripts/Player/HandsMove.cs
@@ -1,31 +1,34 @@
 using UnityEngine;
 
-public class HandsMove : MonoBehaviour
+namespace Player
 {
-    public float speed = 5f;
-    public Rigidbody2D rbRight;
-    public Rigidbody2D rbLeft;
+    public class HandsMove : MonoBehaviour
+    {
+        public float speed = 5f;
+        public Rigidbody2D rbRight;
+        public Rigidbody2D rbLeft;
 
-    private PlayerInputHandler _scriptXY;
-    void Start()
-    {
-        _scriptXY = GetComponent<PlayerInputHandler>();
-    }
+        private PlayerInputHandler _scriptXY;
+        void Start()
+        {
+            _scriptXY = GetComponent<PlayerInputHandler>();
+        }
     
-    void FixedUpdate()
-    {
-        var leftMoveValue = _scriptXY.GetLeftMoveValue();
-        var rightMoveValue = _scriptXY.GetRightMoveValue();
+        void FixedUpdate()
+        {
+            var leftMoveValue = _scriptXY.GetLeftMoveValue();
+            var rightMoveValue = _scriptXY.GetRightMoveValue();
         
-        float horizontalLeft = leftMoveValue.x;
-        float verticalLeft = leftMoveValue.y;
-        float horizontalRight = rightMoveValue.x;
-        float verticalRight = rightMoveValue.y;
+            float horizontalLeft = leftMoveValue.x;
+            float verticalLeft = leftMoveValue.y;
+            float horizontalRight = rightMoveValue.x;
+            float verticalRight = rightMoveValue.y;
         
-        Vector2 moveRight = new Vector2(horizontalRight, verticalRight) * speed;
-        Vector2 moveLeft = new Vector2(horizontalLeft, verticalLeft) * speed;
-        rbRight.linearVelocity = moveRight;
-        rbLeft.linearVelocity = moveLeft;
-    }
+            Vector2 moveRight = new Vector2(horizontalRight, verticalRight) * speed;
+            Vector2 moveLeft = new Vector2(horizontalLeft, verticalLeft) * speed;
+            rbRight.linearVelocity = moveRight;
+            rbLeft.linearVelocity = moveLeft;
+        }
     
+    }
 }
