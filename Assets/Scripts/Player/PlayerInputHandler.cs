b7b3cab954b580a68d761592c075618c82154e02
SofianeLasri <alasri250@gmail.com>
2025-04-15 15:30:04 +0200

    Add input actions for leg movement and grabbing mechanics

--- Assets/Scripts/Player/PlayerInputHandler.cs
+++ Assets/Scripts/Player/PlayerInputHandler.cs
@@ -10,8 +10,12 @@ namespace Player
 
         private InputAction _leftMove;
         private InputAction _rightMove;
+        private InputAction _leftGrab;
+        private InputAction _rightGrab;
         [SerializeField] private Vector2 leftMoveValue;
         [SerializeField] private Vector2 rightMoveValue;
+        [SerializeField] private bool isGrabbingWithLeftHand;
+        [SerializeField] private bool isGrabbingWithRightHand;
     
         // Awake se lance avant Start
         void Awake()
@@ -30,6 +34,14 @@ namespace Player
             _rightMove = _playerInput.currentActionMap.FindAction("RightMove");
             _rightMove.performed += ctx => rightMoveValue = ctx.ReadValue<Vector2>();
             _rightMove.canceled += ctx => rightMoveValue = Vector2.zero;
+            
+            _leftGrab = _playerInput.currentActionMap.FindAction("LeftGrab");
+            _leftGrab.performed += ctx => isGrabbingWithLeftHand = true;
+            _leftGrab.canceled += ctx => isGrabbingWithLeftHand = false;
+            
+            _rightGrab = _playerInput.currentActionMap.FindAction("RightGrab");
+            _rightGrab.performed += ctx => isGrabbingWithRightHand = true;
+            _rightGrab.canceled += ctx => isGrabbingWithRightHand = false;
         }
     
         public Vector2 GetLeftMoveValue()
@@ -40,6 +52,16 @@ namespace Player
         public Vector2 GetRightMoveValue()
         {
             return rightMoveValue;
+        }
+        
+        public bool IsGrabbingWithLeftHand()
+        {
+            return isGrabbingWithLeftHand;
+        }
+        
+        public bool IsGrabbingWithRightHand()
+        {
+            return isGrabbingWithRightHand;
         }
     }
 }
